1156703d53f116f175fc6ff781acdde8e4aca8ed
luizmathias1 <luizmathiasrivabem@gmail.com>
2026-03-21 12:19:04 -0300

    feat: funções para leitura de arquivos .txt e json

---
+++ src/main.py
@@ -0,0 +1,42 @@
+#Integrantes: 
+
+#Alexandre Faisst 
+#Bruno Teider 
+#Luiz Mathias 
+#Rafaela Vecchi 
+ 
+# #Grupo RA1 1
+
+import sys
+import parseExpressao
+import executarExpressao
+from utils import lerArquivo, resetJson
+
+def main(argv=None):
+    argv = argv if argv is not None else sys.argv
+
+    if len(argv) < 2:
+        print("Uso: python src/main.py <nome_arquivo>")
+        sys.exit(1)
+
+    try:
+        arquivo = argv[1]
+        linhas = lerArquivo(arquivo)
+    except Exception as e:
+        print(f"\033[31m{e}\033[0m")
+        sys.exit(1)
+
+    print("\n=== Analisador Léxico ===")
+    for idx, linha in enumerate(linhas, start=1):
+        if not linha.strip(): continue
+
+        try: parseExpressao.parseExpressao(linha, idx)
+        except Exception as e:
+            print(f"Linha {idx}: erro ao analisar: {e}")
+            continue
+    
+    executarExpressao.executarExpressao()
+
+if __name__ == "__main__":
+    resetJson() # Garantir que sempre começamos com um JSON vazio
+    main()
---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+def lerArquivo(arquivo):
+    with open(arquivo, 'r') as f:
+        linhas = f.readlines()
+    
+    print(f"Arquivo '{arquivo}' lido com sucesso. Total de linhas: {len(linhas)}")
+
+    formatado = []
+    for linha in linhas:
+        formatado.append(linha.strip("\n"))
+
+    return formatado
+
+def resetJson():
+    output_path = os.path.join("results", "tokens.txt")
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)  # garante que 'results/' exista
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump({"entries": []}, f, ensure_ascii=False, indent=2)
+
+
+def ler_json():
+    output_path = os.path.join("results", "tokens.txt")
+    with open(output_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data["entries"]
